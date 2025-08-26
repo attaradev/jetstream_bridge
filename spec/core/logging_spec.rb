51e78313bf4defb39ac8551748a0eab7cf1d322f
Mike Attara <mpyebattara@gmail.com>
2025-08-26 09:06:17 +0000

    Add debug logging helper

--- spec/core/logging_spec.rb
+++ spec/core/logging_spec.rb
@@ -17,4 +17,15 @@ RSpec.describe JetstreamBridge::Logging do
     io.rewind
     expect(io.string).to include('[Spec] hello')
   end
+
+  it 'logs debug messages' do
+    io = StringIO.new
+    custom_logger = Logger.new(io)
+    JetstreamBridge.configure(logger: custom_logger)
+
+    described_class.debug('dbg', tag: 'Spec')
+
+    io.rewind
+    expect(io.string).to include('[Spec] dbg')
+  end
 end
